4a683e44c7e6da1a0ada3e191ae122141d4edc03
= <=>
2024-07-23 17:53:23 +0530

    initial commit

---
+++ src/app/home/app.home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-home',
+    templateUrl:'./app.home.component.html',
+    styleUrl:'./app.home.component.scss',
+})
+
+export class HomeComponent{
+
+}
